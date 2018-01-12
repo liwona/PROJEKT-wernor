50d1ffdd572621525c859463a77c514891e42b21
liwona <liwona@users.noreply.github.com>
2018-01-12 15:42:26 +0100

    Changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 //komentarz obejmuje tylko linie
 /* to też jest komentarz*/
 
-$(document).ready(function(){
-	//console.log('Dupcia jeża');
+//$(document).ready(function(){
+	//console.log('Wernor');
+//});
+
+/* Dynamic top menu positioning
+ *
+ */
+
+var num = 0; //number of pixels before modifying styles
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('.menu').addClass('fixed');
+    } else {
+        $('.menu').removeClass('fixed');
+    }
 });
+
+//USE SCROLL WHEEL FOR THIS FIDDLE DEMO
